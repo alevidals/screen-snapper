3cf298a2847171fd13892bdecc9747e2c3f28fda
alevidals <alejandro.vidal.sanchez16@gmail.com>
2024-08-13 21:35:00 +0200

    feat: add possibility to download the recorded video

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,6 @@
 export const MIME_TYPE = "video/webm";
+
+export const BUTTONS_HTML = {
+  recording: `Recording... <div class="ml-4 h-2 w-2 rounded-full bg-red-400 animate-ping" />`,
+  startRecording: "Start recording",
+};
