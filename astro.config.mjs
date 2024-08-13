b1aebed182c843858223a5387330ac4fa98f58cc
Alejandro Vidal <alejandro.vidal.sanchez16@gmail.com>
2024-08-13 23:53:03 +0200

    chore: read base url from environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  experimental: {
+    env: {
+      schema: {
+        BASE_URL: envField.string({
+          context: "client",
+          access: "public",
+        }),
+      },
+    },
+  },
 });
